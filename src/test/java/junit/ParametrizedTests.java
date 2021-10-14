cad53bd1bd7ace470d59172dfc1411eaa996eb39
nataliaado <nataliaado@hotmail.com>
2021-10-14 18:56:56 -0300

    Implementação de Testes parametrizados para vários valores constantes com @CsvSource

--- src/test/java/junit/ParametrizedTests.java
+++ src/test/java/junit/ParametrizedTests.java
@@ -1,6 +1,8 @@
 package junit;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParametrizedTests {
@@ -10,4 +12,37 @@ public class ParametrizedTests {
     void intValues(int theParam) {
         System.out.println("theParam = " + theParam);
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"firstString", "secondString"})
+    void stringValues(String theParam) {
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,rogers", "captain,marvel", "bucky,barnes"})
+        // Csv = Comma-separated values
+    void csvSource_StringString(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,32,true", "captain,21,false", "bucky,5,true"})
+    void csvSource_StringIntBoolean(String param1, int param2, boolean param3) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america,'steve,rogers'", "winter soldier,'bucky,barnes'"})
+    void csvSource_StringWithComa(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+    void csvSource_StringWithDiffDelimiter(String param1, String param2) {
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
 }
